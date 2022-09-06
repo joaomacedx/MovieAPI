8af057a54c3eccdf6b2687371800c0e61fd8f694
João Macêdo <99701121+joaomacedx@users.noreply.github.com>
2022-09-06 20:16:53 -0300

    Getting movie data

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using MovieAPI.Models;
+
+namespace MovieAPI.Controllers
+{
+    [ApiController]
+    [Microsoft.AspNetCore.Mvc.Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        private static List<Movie> movies = new List<Movie>();
+
+        [HttpPost]
+        public void PostMovie([FromBody] Movie movie)
+        {
+            movies.Add(movie);
+            Console.WriteLine(movie.Title);
+        }
+
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieAPI.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string Genre { get; set; }
+        public int Duration { get; set; }
+    }
+}
